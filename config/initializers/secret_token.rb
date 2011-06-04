d61b2bd59ce99426a6cb473c36a1b238191eb18b
Andreas <andreas@andreas-laptop.(none)>
2011-06-04 14:01:56 +0200

    initialer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyComments::Application.config.secret_token = '243c501092666c709b81051304b70e78a2d47b569e9758783034f4803b67274f5764ae6e5f1dbc41134c8b7f514919f900c7499375a5ea431878b417c8103117'
